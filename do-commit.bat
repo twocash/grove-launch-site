68d66eae9ae79a62af4933595ada170329ce2169
twocash <jimcalhoun@gmail.com>
2026-04-08 10:29:48 -0400

    rename profiles to architectural categories, add site navigation paragraph to home identity block

--- do-commit.bat
+++ do-commit.bat
@@ -1,3 +1,3 @@
 cd /d C:\GitHub\grove-launch-site
 git add -A
-git commit -m "standings: reframe tiers as phase states, move profiles up, add landscape context, ground methodology intro"
+git commit -m "rename profiles to architectural categories, add site navigation paragraph to home identity block"
