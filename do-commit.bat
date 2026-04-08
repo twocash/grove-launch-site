f2455b6461c4c93f4524148329969cad69f2339c
twocash <jimcalhoun@gmail.com>
2026-04-08 12:26:00 -0400

    standards-001-wip-sections-I-through-V-complete

--- do-commit.bat
+++ do-commit.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d C:\GitHub\grove-launch-site
 git add -A
-git commit -m "update-provenance-sentence-add-venturebeat-canonical-link"
+git commit -m "standards-001-wip-sections-I-through-V-complete"
