dd79f3b19aee149a5916f27062c2b826e22f8590
twocash <jimcalhoun@gmail.com>
2026-04-08 13:58:26 -0400

    Add scroll cue to observations, standings, papers, about pages

--- do-commit.bat
+++ do-commit.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d C:\GitHub\grove-launch-site
 git add -A
-git commit -m "Static amber scroll cue, keyboard arrow navigation on all pages"
+git commit -m "Add scroll cue to observations, standings, papers, about pages"
