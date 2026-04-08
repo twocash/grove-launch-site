cf9edf6af25d51cd81467b15cbeac9053ca2503f
twocash <jimcalhoun@gmail.com>
2026-04-08 10:37:04 -0400

    rename landscape heading, remove CTAs from phase state tiers

--- do-commit.bat
+++ do-commit.bat
@@ -1,3 +1,3 @@
 cd /d C:\GitHub\grove-launch-site
 git add -A
-git commit -m "rename profiles to architectural categories, add site navigation paragraph to home identity block"
+git commit -m "rename landscape heading, remove CTAs from phase state tiers"
