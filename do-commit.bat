4ac384df1feed312cd497afaf359ffc62ae75b56
twocash <jimcalhoun@gmail.com>
2026-04-08 12:12:55 -0400

    update-provenance-sentence-add-venturebeat-canonical-link

--- do-commit.bat
+++ do-commit.bat
@@ -1,5 +1,4 @@
 @echo off
 cd /d C:\GitHub\grove-launch-site
-del fix-nav.py
 git add -A
-git commit -m "clickable-logo-add-papers-nav-link-all-pages"
+git commit -m "update-provenance-sentence-add-venturebeat-canonical-link"
