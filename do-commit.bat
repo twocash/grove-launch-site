dd0a0ed90f0581e3927fdbbb351f87fc86cb3121
twocash <jimcalhoun@gmail.com>
2026-04-08 12:04:53 -0400

    clickable-logo-add-papers-nav-link-all-pages

--- do-commit.bat
+++ do-commit.bat
@@ -1,4 +1,5 @@
 @echo off
 cd /d C:\GitHub\grove-launch-site
+del fix-nav.py
 git add -A
-git commit -m "complete-telemetry-trap-page-add-docs-dir"
+git commit -m "clickable-logo-add-papers-nav-link-all-pages"
